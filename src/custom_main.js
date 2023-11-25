89f48942a54b5dc6269679137f69e3fec9ab88b8
jkep2004 <97905011+jkep2004@users.noreply.github.com>
2023-11-25 13:48:13 +0000

    Added custom block sizes and filenames

--- src/custom_main.js
+++ src/custom_main.js
@@ -1,21 +1,20 @@
-var blocksize = 32;
+var blocksize = document.getElementById("blocksize_inp");
 
 function open_files () {
     var inp = document.getElementById("get-files");
     var output = []
-
-    console.log(inp.files)
+    var filenames = []
     
     // Access and handle the files 
     for (let i = 0; i < inp.files.length; i++) {
         let file = inp.files[i];
         output.push(URL.createObjectURL(file))
+        filenames.push(file.name.split(".")[0])
     }
 
-    pxm = new pixelit_multi (output, blocksize)
+    pxm = new pixelit_multi (output, filenames, blocksize.value)
     pxm.multi()
 
     return output
 }
 
-var pxm = new pixelit_multi ([], blocksize)
